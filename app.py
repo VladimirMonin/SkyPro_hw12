95766f3af65bba894ba63a43e8e02732b8d771e0
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-25 17:24:00 +0600

    chore: Шаг 4. Мелкие правки (все кнопки назад и добавить пост теперь работают). Вьюшка на добавление создана. Надо настраивать!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, send_from_directory
 
 from main.views import main_blueprint
+from loader.views import loader_blueprint
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -8,6 +9,7 @@ UPLOAD_FOLDER = "uploads/images"
 app = Flask(__name__)
 
 app.register_blueprint(main_blueprint, url_prefix="/")
+app.register_blueprint(loader_blueprint, url_prefix="/loader/")
 
 # @app.route("/")
 # def page_index():
--- loader/views.py
+++ loader/views.py
@@ -8,3 +8,8 @@ from flask import render_template
 # Рожаем новый блюпринт, выбираем ему имя :)
 # Добавляем настройку кастомной папки с шаблонами
 loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates')
+
+# Создаем вьюшку тестовую страницы (в декораторе блюпринт а не app!)
+@loader_blueprint.route('/')
+def main():
+    return render_template('post_form.html')
