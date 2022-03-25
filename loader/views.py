50b9fce70f60196a8745a4a6725265664f41e3ae
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-25 16:58:18 +0600

    chore: Шаг 4. Сделал бюпринт loader

--- loader/views.py
+++ loader/views.py
@@ -4,3 +4,7 @@ from flask import Blueprint, request
 
 # Импортируем шаблонизатор
 from flask import render_template
+
+# Рожаем новый блюпринт, выбираем ему имя :)
+# Добавляем настройку кастомной папки с шаблонами
+loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates')
