78547c00bbbd749d0f9ed76e928a4cad9dc99879
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-25 15:57:55 +0600

    docs: Прокомментировал действия. Добавил некоторые вопросы к НАСТАВНИКУ ;)

--- main/views.py
+++ main/views.py
@@ -14,15 +14,14 @@ main_blueprint = Blueprint('main_blueprint', __name__, template_folder='template
 # Создаем вьюшку главной страницы (в декораторе блюпринт а не app!)
 @main_blueprint.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html') # ВОПРОС. Почему у нас в домашке на главной странице вместо ленты постов форма ПОИСКА???!
 
 
 # Создаем вьюшку страницы поисковой выдачи (post_list.html)
 @main_blueprint.route('/search/')
 def search_page():
-    search_str = request.args.get('s')
-    posts_list = load_posts_json()
-    find_posts = search_post(posts_list, search_str)
-    post_count = len(posts_list)
-    # Тут надо функцию поиска по json нужных постов
+    search_str = request.args.get('s')  # Получаем поисковый запрос из урла
+    posts_list = load_posts_json()  # Загружаем список постов из файла. Сделал тут, т.к. посты ведь будут появлятся новые. Чтобы данные были сввежими
+    find_posts = search_post(posts_list, search_str)  # Производим поиск поста
+    post_count = len(posts_list)  # Длина списка (для доп. фитч)
     return render_template('post_list.html', posts=find_posts, count=post_count, search_str=search_str)  # Наверное стоит сделать условие по длине списка? (Посты не найдены)
