e684fd641d925d59192fd306209a867f762e69da
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-25 14:00:05 +0600

    chore: Сделал функцию поиска постов

--- functions.py
+++ functions.py
@@ -14,5 +14,11 @@ def load_post_json(filename='posts.json'):
 
 
 def search_post(posts_list, search_post):
+# ВОПРОС. В КАКОМ ВАРИАНТЕ ПРАВИЛЬНО ЭТО ДЕЛАТЬ? Выдавать во вьюшку готовый результат, или весь список, а поиск делать циклом в HTML?
+# В ЛЮБОМ ли варианте обработка будет на сервере? Или если отдать в HTML - весь json пойдет в браузер юзера и там будет перебор циклом? Это безопасно?
     """Функция принимает список постов и производит поиск по вхождению строки"""
-    pass
+    result_list = []
+    for post in posts_list:
+        if search_post.lower() in post['content'].lower():
+            result_list.append(post)
+    return result_list
