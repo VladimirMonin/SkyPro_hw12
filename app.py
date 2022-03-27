98f0f4689afc866e6690d883bf7614fa7d48b83f
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-27 23:59:49 +0600

    chore: ХЗ что тут...

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(main_blueprint, url_prefix="/")
 app.register_blueprint(loader_blueprint, url_prefix="/")
 
-@app.route(f'{IMAGES_FOLDER}<path:path>')
+@app.route(f'/loaded/<path:path>/')
 def img_dir(path):
     return send_from_directory(IMAGES_FOLDER, path)
 
--- loader/loader_views.py
+++ loader/loader_views.py
@@ -8,7 +8,7 @@ from flask import render_template
 
 # Рожаем новый блюпринт, выбираем ему имя :)
 # Добавляем настройку кастомной папки с шаблонами
-loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates', url_prefix='/post')
+loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates', url_prefix='/post/')
 
 
 # Создаем вьюшку тестовую страницы (в декораторе блюпринт а не app!)
